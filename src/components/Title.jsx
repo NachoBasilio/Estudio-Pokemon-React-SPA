9a5f63274f57481851d4a8d7b11b68dd03062e19
IgnaDev <arcortoon@gmail.com>
2021-02-12 18:17:27 -0300

    Media querys :)

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ import "./styles/Title.css"
 const Title = () => {
     return (
         <div className="Titulos">
-            <h1 className="Titulos__Bienvenida">Bienvenido</h1>
+            <h1 className="Titulos__Bienvenida">Bienvenido.</h1>
             <p className="Titulos__Parrafo">Este es tu creador de pokemon personal.</p>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Homepage from "./pages/homepage"
 import "./styles/index.css"
+import "./styles/media.css"
 
 const container = document.getElementById("root")
 
